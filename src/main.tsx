15259c586e0224c4848786f5dddf5d6c34a2baa2
Janith-Chamikara <janithchamikara13@gmail.com>
2023-11-17 21:51:17 +0530

    added authProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import theme from "./theme";
+import AuthContextProvider from "./context/AuthContext";
 
 const rootElement: HTMLElement | null = document.getElementById("root");
 const rootElementAsHTMLElement = rootElement as HTMLElement;
 ReactDOM.createRoot(rootElementAsHTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
