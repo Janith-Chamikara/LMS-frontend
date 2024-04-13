1cadedd6c63a7a9ab2ad19284b8e423882383380
Janith-Chamikara <janithchamikara13@gmail.com>
2024-04-13 10:31:15 +0530

    updated

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.VITE_API_PATH;
+const BACKEND_BASE_URL = import.meta.env.VITE_API_PATH;
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: BACKEND_BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BACKEND_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
