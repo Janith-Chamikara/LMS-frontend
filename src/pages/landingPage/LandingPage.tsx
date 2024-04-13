84e36c4635d2c6b4f3e467c0b2b4b701f70bbe3d
Janith Chamikara <janithchamikara13@gmail.com>
2024-04-13 19:20:54 +0530

    Update LandingPage.tsx

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -46,7 +46,7 @@ const LandingPage: FC = () => {
       <Flex
         width={"90vw"}
         mx={"auto"}
-        height={"100dvh"}
+        height={"100vh"}
         direction={{ base: "column-reverse", lg: "row" }}
         alignItems={"center"}
         justifyContent={"center"}
